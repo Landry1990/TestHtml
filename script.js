acdbebe5dc745380f874819db961c61df5a8fb90
Landry1990 <nkahako@yahoo.fr>
2025-04-20 09:26:27 +0100

    20/04/2025

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 const images = document.querySelectorAll(".gallery");
-const imageContainer = document.getElementById("#image-container");
+const imageContainer = document.getElementById(".lightbox");
 
 // Ajoute un écouteur d'événement à chaque image
-images.forEach(image => {
-    image.addEventListener("click", () => {
+images.forEach(item => {
+    item.addEventListener("click", () => {
         // Crée une nouvelle balise image avec l'image cliquée
-        imageContainer.innerHTML = `<img src="${image.src}" alt="Image affichée">`;
+        imageContainer.innerHTML = `<img src="images/${item.src}" alt="Image affichée">`;
     });
 });
